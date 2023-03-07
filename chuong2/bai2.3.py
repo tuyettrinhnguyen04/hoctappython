6c40c339397b163352b182e7b6de6d0c0a15e7db
tuyettrinhnguyen04 <127210330+tuyettrinhnguyen04@users.noreply.github.com>
2023-03-07 20:16:58 +0700

    day bai tap chuong 2 len github

---
+++ chuong2/bai2.3.py
@@ -0,0 +1,4 @@
+p=float(input('tien von ban đau'))
+r=float(input('lãi suat'))
+n=float(input('so thang gui'))
+print('tien lanh cuoi ky:',p*(1+r*n))
---
+++ chuong2/bai2.4.py
@@ -0,0 +1,6 @@
+import math
+a=float(input('a'))
+b=float(input('b'))
+c=float(input('c'))
+s=((a+b+c)/2)
+print('dien tich tam giac:','sqrt(s*(s-a)*(s-b)*(s-c))',math.sqrt(s*(s-a)*(s-b)*(s-c)))
