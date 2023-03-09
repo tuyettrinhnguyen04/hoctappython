7ce6c9230173bedd780b941b74711b1e5f770869
tuyettrinhnguyen04 <127210330+tuyettrinhnguyen04@users.noreply.github.com>
2023-03-10 05:55:29 +0700

    chuong4

---
+++ chuong3/slide chuong 3/3.1.py
@@ -0,0 +1,15 @@
+a=float(input('a='))
+b=float(input('b='))
+c=float(input('C='))
+max=a
+if max<b:
+    print('SLN=',b)
+if max<c:
+    print('SLN=',c)
+min=a
+if min>b:
+    print('SBN=',b)
+if min>c:
+    print('SBN=:',c)
+
+    
